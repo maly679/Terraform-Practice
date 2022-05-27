f3955198af503f3bdc7a858b99f6bac9daeaf9be
maly679 <46073520+maly679@users.noreply.github.com>
2022-05-27 08:34:19 -0600

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,25 @@
+##Cluster requires two subnets in different AZs for it to work
+##Requires IAM policies
+##Reuquires Cloudwatch logs
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "example"
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks-cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
+}
